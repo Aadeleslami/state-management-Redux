a5203e55a18c056c8e780b6575f1632b30e1dc37
Adel <adeleslami0077@yahoo.com>
2025-04-02 23:28:57 +0330

    add first reducer

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeContainer() {
+  return (
+  <div>
+      <h2>Cake Number: 10</h2>
+      <button >BUY CAKE</button>
+  </div>
+  )
+}
+
+export default CakeContainer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
 import cakeReducer from "./cake/cakeReducer";
 
- const store =createStore(cakeReducer)
- export default store
+const store =createStore(cakeReducer)
+export default store
